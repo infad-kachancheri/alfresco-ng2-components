07a7440bd8b818b073b3d8a06d79ec404a37ec64
Infad Kachancheri <infad.k@muraai.com>
2017-04-27 12:48:43 +0530

    on_click_emitter_for_heading
    on click emitter for heading element

--- ng2-components/ng2-alfresco-core/src/components/collapsable/accordion-group.component.ts
+++ ng2-components/ng2-alfresco-core/src/components/collapsable/accordion-group.component.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-import { Component, Input, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy, Output, EventEmitter } from '@angular/core';
 import { AccordionComponent } from './accordion.component';
 
 @Component({
@@ -34,6 +34,9 @@ export class AccordionGroupComponent implements OnDestroy {
 
     @Input()
     headingIcon: string;
+
+    @Output()
+    onHeadingSelection: EventEmitter<any> = new EventEmitter<any>();
 
     @Input()
     set isOpen(value: boolean) {
@@ -75,5 +78,9 @@ export class AccordionGroupComponent implements OnDestroy {
 
     getAccordionIcon(): string {
         return this.isOpen ? 'expand_less' : 'expand_more';
+    }
+
+    onHeadingClick() {
+        this.onHeadingSelection.emit(this.heading);
     }
 }
